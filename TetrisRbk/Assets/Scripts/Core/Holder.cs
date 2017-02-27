53dd53f59d616dcd7d76cbbd129569c5bd03fbe8
xroba <xroba@rubbik.com>
2017-02-27 10:05:52 +0100

    add hold

---
+++ TetrisRbk/Assets/Scripts/Core/Holder.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Holder : MonoBehaviour {
+
+    private float scale = 0.5f;
+    public Transform xFormHolder;
+    public Shape holdShape;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public bool HasShape() {
+        return holdShape != null ? true : false;
+    }
+
+    public void PutShapeOnHold(Shape shape)
+    {
+        holdShape = shape;
+        holdShape.transform.position = xFormHolder.transform.position;
+        holdShape.transform.localScale = new Vector3(scale,scale,scale);
+
+    }
+
+    public Shape RetrieveHoldShape()
+    {
+        return holdShape;
+    }
+
+
+}
