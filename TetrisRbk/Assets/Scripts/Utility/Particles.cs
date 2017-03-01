7a2938aec75bfbd3aec5c39fbac0520b076455ce
xavier roba <xroba@rubbik.com>
2017-03-01 09:28:16 +0100

    particles

---
+++ TetrisRbk/Assets/Scripts/Utility/Particles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particles : MonoBehaviour {
+
+    public ParticleSystem[] allParticles;
+
+	// Use this for initialization
+	void Start () {
+        allParticles = GetComponentsInChildren<ParticleSystem>();
+	}
+	
+
+    public void PlayParticle()
+    {
+        foreach(ParticleSystem ps in allParticles)
+        {
+            ps.Stop();
+            ps.Play();
+        }
+    }
+
+}
